6c19196225e2e29512e79400124477ed81b6dc15
KHARERISHABH <rishabkhare41@gmail.com>
2023-04-19 21:44:12 +0530

    add to cart fixes

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch()
+  const { title, quantity, total, price, id } = props.item;
+  console.log(props.item)
+  const addItemHandler = () => {
+    dispatch(cartActions.addItemToCart({
+      id, title, price
+    }))
+  }
+  const removeCartHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id))
+  }
 
   return (
     <li className={classes.item}>
@@ -17,8 +29,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeCartHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
